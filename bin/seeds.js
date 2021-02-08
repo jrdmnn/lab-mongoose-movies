33184ddad73675c551ce5ca96ee2570662697cb1
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-02-08 15:59:15 +0100

    add celebrity model

---
+++ bin/seeds.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Celebrity = require('../models/Celebrity.model');
+
+const DB_NAME = 'lab-mongoose-movies'
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const celebrities = [
+  {
+    name: 'Matthew McConaughey',
+    ocupation: 'Actor',
+    catchPhrase: 'Alright, Alright, Alright'
+  },
+  {
+    name: 'Sheldon Cooper',
+    ocupation: 'Actor',
+    catchPhrase: 'Bazinga!'
+  },
+  {
+    name: 'Heidi Klum',
+    ocupation: 'Model/Presenter',
+    catchPhrase: 'Auf wiedersehen!'
+  },
+  {
+    name: 'Oprah Winfrey',
+    ocupation: 'Talk Show Host',
+    catchPhrase: 'Aha Moment!'
+  },
+
+]
+Celebrity.create(celebrities)
+  .then(celebritiesFromDB => {
+    console.log(`Created ${celebritiesFromDB.length} celebrities`);
+ 
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while creating celebrities from the DB: ${err}`));
---
+++ models/Celebrity.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema({
+  name: {
+    type: String
+  },
+  occupation: {
+    type: String
+  },
+  catchPhrase: {
+    type: String
+  }
+})
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
