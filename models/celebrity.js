c305b667401cb10acf4fa6963d8539589c8868a9
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-09 18:32:25 +0100

    iteration 7 done

--- models/celebrity.js
+++ models/celebrity.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const celebritySchema = new Schema({
   name: String,
   occupation: String,
-  catchPhrase: String,
+  catchPhrase: String
 });
 
 const Celebrity = mongoose.model("Celebrity", celebritySchema);
