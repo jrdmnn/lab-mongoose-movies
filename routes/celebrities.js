0880bf803807c652c35834664695eb158772ca66
Seamus O'Sullivan <info@seamuso.com>
2021-02-09 20:33:40 +0100

    8ish

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -11,24 +11,11 @@ router.get("/celebrities", (req, res, next) => {
 });
 
 router.get('/celebrities/new', (req, res) => {
+  Celebrity.find().then(celebFromDB => {
   res.render('celebrities/new', { CelList4: celebFromDB });
-})
+ })
+});
 
-
-router.post('/celebrities', (req, res) => {
-  console.log(req.body)
-  const name = req.body.name;
-  const occupation = req.body.occupation;
-  const catchPhrase = req.body.catchPhrase;
-  Celebrity.create({
-    name: name,
-    occupation: occupation,
-    catchPhrase: catchPhrase
-  }) 
-  .then(celeb => res.redirect('/celebrities'))
-  .catch(err=> console.log(err))
-     
-})
 
 router.get('/celebrities/:id', (req, res) => {
   const celebId = req.params.id;
@@ -40,6 +27,35 @@ router.get('/celebrities/:id', (req, res) => {
   .catch(err => {
     console.log(err);
   })
+})
+
+
+router.post('/celebrities', (req, res) => {
+  console.log(req.body)
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  const catchPhrase = req.body.catchPhrase;
+  Celebrity.create({
+    name,
+    occupation,
+    catchPhrase
+  }) 
+  .then(celeb => {
+    console.log('This celeb was added: ', celeb);
+    res.redirect(`/celebrities`)
+ })
+ .catch(err=> console.log(err)) 
+})
+
+
+router.post('/celebrities/:id/delete', (req, res) => {
+  const celebId = req.params.id;
+  Celebrity.findByIdAndDelete(celebId)
+  .then(celebsFDB => {
+    
+    res.redirect(`/celebrities`)
+  })
+  .catch(err => console.log(err))
 })
 
 
---
+++ routes/movies.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const router = express.Router()
+const Movie = require('../models/Movie')
+const Celebrity = require('../models/Celebrity')
+
+router.get("/movies", (req, res) => {
+  Movie.find().then(movieFDB => {
+    //console.log(celebsFDB)
+    res.render('movies/index.hbs', {movieFDB})
+  })
+});
+
+router.get('/movies/new', (req, res) => {
+Celebrity.find().then(celebsFDB => {
+  console.log('FUCKTHIS', celebsFDB)
+  res.render('movies/new.hbs', {celebList : celebsFDB})
+  })
+});
+
+router.post('/movies', (req, res) => {
+  console.log('BODY', req.body)
+  const {name, genre, plot, cast} = req.body;
+  Movie.create({
+    name,
+    genre,
+    plot,
+    cast,
+  }) 
+  .then(() => {
+    res.redirect(`/movies`)
+  
+})
+.catch(err => console.log(err))
+})
+
+
+module.exports = router;
