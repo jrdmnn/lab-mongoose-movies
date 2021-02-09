ea8171e10a886d1b3a51d7ddd598a75c6a1b2661
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-09 16:51:49 +0100

    test that everything works so far, link those routes that were missing and change name that needed to be changed on show.hbs. Now iterations from Monday are indeed ready

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const index = require("./routes/index");
 app.use("/", index);
 
 const celebrities = require("./routes/celebrities")
+app.use("/", celebrities);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
