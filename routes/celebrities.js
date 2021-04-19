2c25405ef8824707dda6c19d0868374b5f9c6001
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-19 19:33:48 +0200

    add details

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { findById } = require("../models/Celebrity");
 const Celebrity = require("../models/Celebrity");
 
 router.get('/celebrities', (req,res,next)=>{
@@ -11,5 +12,18 @@ router.get('/celebrities', (req,res,next)=>{
     next(err); 
   })
 });
+
+router.get('/celebrities/:id',(req,res,next)=>{
+  console.log(req.params.id);
+  const celebId = req.params.id;
+  Celebrity.findById(celebId)
+  .then(celeb => {
+    console.log("okay",celeb);
+    res.render('celebrities/show', {celebInfo : celeb})
+  })
+  .catch(err=>{
+    next(err);
+  })  
+})
 
 module.exports = router; 
