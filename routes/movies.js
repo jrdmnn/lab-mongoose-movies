b535d951f362cfe47218a826b07f877e4976e4bd
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-20 14:33:32 +0200

    add new functionality

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity");
 const {
   findById
 } = require("../models/Movie");
 const Movie = require("../models/Movie");
 
-router.get('/movies/new', (req,res)=>{
-  res.render('movies/new')
+router.get('/movies/new', (req,res,next)=>{
+  Celebrity.find()
+  .then(celebsInDb => {
+    res.render('movies/new', {celebrities : celebsInDb});
+  })
+  .catch(err=>{
+    next(err);
+  })
 })
 
 module.exports = router;
