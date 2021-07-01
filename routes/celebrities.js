2563c977d7539f85f578adfb77227351842f0e92
Raoul Ullens <raoulullens@live.com>
2021-07-01 14:41:02 +0200

    cannot display details

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { findById } = require("../models/Celebrity");
 // ../ to go up one folder
 const Celebrity = require('../models/Celebrity');
 
@@ -9,12 +10,35 @@ router.get('/celebrities', (req, res, next) => {
     // do I need to declare a const here?
     // How to do the below? 
     // Pass the array of celebrities into the view as a variable.
-    console.log(allCelebrities);
+    // console.log(allCelebrities);
     res.render('celebrities/index', {celebrities: allCelebrities});
   })
   .catch(err => {
     console.log(err)
   })
 })
+
+
+// router.get('celebrities/new', (req, res, next) => {
+//   .then(allCelebrities => {
+//     res.render('celebrities/new', {celebrities: allCelebrities});
+//   }
+//   .catch(err => {
+//     console.log(err)
+//   })
+// })
+
+router.get('celebrities/:id', (req, res, next) => {
+  console.log(req.params.id);
+  Celebrity.findById(req.params.id)
+    .then(allCelebrities => {
+      res.render('celebrities/show.hbs', { allCelebrities });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
 
 module.exports = router;
